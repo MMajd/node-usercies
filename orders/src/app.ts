78715043f3afd8e0bcd3ce61f3c901a2a8a77c15
Majd <majd@local.com>
2022-05-21 15:58:06 +0200

    adding some comments

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,23 +1,24 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@sgtickets/common";
 
-import { deleteOrderRouter } from './routes/delete';
-import { indexOrderRouter } from './routes/index';
-import { newOrderRouter } from './routes/new';
-import { showOrderRouter } from './routes/show';
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes/index";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
+// first of all make that the user is logged in
 app.use(currentUser);
 
 app.use(deleteOrderRouter);
@@ -25,7 +26,7 @@ app.use(indexOrderRouter);
 app.use(newOrderRouter);
 app.use(showOrderRouter);
 
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
